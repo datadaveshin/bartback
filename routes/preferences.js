1d07d0597eeb54b813789fcc4485821b0466538f
David Shin <davidshin444@gmail.com>
2017-02-10 17:25:00 -0800

    Again, got the info from login/ to script.js

--- routes/preferences.js
+++ routes/preferences.js
@@ -73,31 +73,31 @@ router.post('/', (req, res) => {
 // })
 
 // Receive from Registration Page
-router.post('/example', (req, res) => {
-    const password = req.body.password1;
-    bcrypt.hash(password, 12)
-    .then((hashed) => {
-
-        let newUser = {
-            userName: req.body.userName,
-            email: req.body.email,
-            homeStation: req.body.homeStation,
-            awayStation: req.body.awayStation,
-            hashedPassword: hashed
-        };
-        console.log("\n\n\n\n\n\n######## IN HERE ###############")
-        console.log("newUser", newUser)
-        console.log("######## IN HERE ###############\n\n\n\n\n\n")
-
-        return knex('users')
-            .insert(decamelizeKeys(newUser), ['id','user_name', 'email', 'home_station', 'away_station'])
-    })
-    .then(()=>{
-        res.redirect('/auth/login')
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    });
-});
+// router.post('/example', (req, res) => {
+//     const password = req.body.password1;
+//     bcrypt.hash(password, 12)
+//     .then((hashed) => {
+//
+//         let newUser = {
+//             userName: req.body.userName,
+//             email: req.body.email,
+//             homeStation: req.body.homeStation,
+//             awayStation: req.body.awayStation,
+//             hashedPassword: hashed
+//         };
+//         console.log("\n\n\n\n\n\n######## IN HERE ###############")
+//         console.log("newUser", newUser)
+//         console.log("######## IN HERE ###############\n\n\n\n\n\n")
+//
+//         return knex('users')
+//             .insert(decamelizeKeys(newUser), ['id','user_name', 'email', 'home_station', 'away_station'])
+//     })
+//     .then(()=>{
+//         res.redirect('/auth/login')
+//     })
+//     .catch(err => {
+//         res.status(400).send(err);
+//     });
+// });
 
 module.exports = router;
